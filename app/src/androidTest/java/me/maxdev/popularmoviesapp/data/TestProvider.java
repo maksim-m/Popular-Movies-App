68a03033759a54f846e2115bcae0044dc2ae6da6
Maksim Moiseikin <m.moiseikin@gmail.com>
2016-04-06 23:25:48 +0200

    Register the ContentProvider

--- app/src/androidTest/java/me/maxdev/popularmoviesapp/data/TestProvider.java
+++ app/src/androidTest/java/me/maxdev/popularmoviesapp/data/TestProvider.java
@@ -1,5 +1,9 @@
 package me.maxdev.popularmoviesapp.data;
 
+import android.content.ComponentName;
+import android.content.pm.PackageManager;
+import android.content.pm.ProviderInfo;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.test.AndroidTestCase;
 
@@ -9,6 +13,46 @@ public class TestProvider extends AndroidTestCase {
     protected void setUp() throws Exception {
         super.setUp();
         deleteAllRecords();
+    }
+
+    public void testProviderRegistry() {
+        PackageManager pm = mContext.getPackageManager();
+
+        // We define the component name based on the package name from the context and the
+        // MoviesProvider class.
+        ComponentName componentName = new ComponentName(mContext.getPackageName(),
+                MoviesProvider.class.getName());
+        try {
+            // Fetch the provider info using the component name from the PackageManager
+            // This throws an exception if the provider isn't registered.
+            ProviderInfo providerInfo = pm.getProviderInfo(componentName, 0);
+
+            // Make sure that the registered authority matches the authority from the Contract.
+            assertEquals("Error: MoviesProvider registered with wrong authority " + providerInfo.authority,
+                    providerInfo.authority, MoviesContract.CONTENT_AUTHORITY);
+        } catch (PackageManager.NameNotFoundException e) {
+            // I guess the provider isn't registered correctly.
+            assertTrue("Error: MoviesProvider not registered at " + mContext.getPackageName(),
+                    false);
+        }
+    }
+
+    public void deleteAllRecordsFromProvider() {
+        mContext.getContentResolver().delete(
+                MoviesContract.MovieEntry.CONTENT_URI,
+                null,
+                null
+        );
+
+        Cursor cursor = mContext.getContentResolver().query(
+                MoviesContract.MovieEntry.CONTENT_URI,
+                null,
+                null,
+                null,
+                null
+        );
+        assertEquals("Error: Records not deleted from Movies table during delete", 0, cursor.getCount());
+        cursor.close();
     }
 
     /*
