b4a16db5714be7d91bcd32bb91ee1f69cf438bcc
Maksim Moiseikin <m.moiseikin@gmail.com>
2016-04-24 21:23:55 +0200

    Fix grid items layout

--- app/src/main/java/me/maxdev/popularmoviesapp/ui/movies/MoviesAdapter.java
+++ app/src/main/java/me/maxdev/popularmoviesapp/ui/movies/MoviesAdapter.java
@@ -40,6 +40,7 @@ public class MoviesAdapter extends CursorRecyclerViewAdapter<MovieGridItemViewHo
                     .load(POSTER_IMAGE_BASE_URL + POSTER_IMAGE_SIZE + movie.getPosterPath())
                     .placeholder(new ColorDrawable(context.getResources().getColor(R.color.accent_material_light)))
                     .diskCacheStrategy(DiskCacheStrategy.SOURCE)
+                    .centerCrop()
                     .crossFade()
                     .into(viewHolder.moviePoster);
         }
